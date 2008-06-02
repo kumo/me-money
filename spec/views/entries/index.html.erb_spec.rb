247c2e2e346b273baa445182d113aa79796fa92f
kumo <kumo@kumo.it>
2008-06-03 00:14:50 +0200

    specs were failing because account mock didn't include name

--- spec/views/entries/index.html.erb_spec.rb
+++ spec/views/entries/index.html.erb_spec.rb
@@ -5,25 +5,25 @@ describe "/entries/index.html.erb" do
   
   before(:each) do
     entry_98 = mock_model(Entry)
-    entry_98.should_receive(:description).and_return("MyString")
+    entry_98.should_receive(:description).and_return("EntryDescription")
     entry_98.should_receive(:paid_on).and_return(Date.today)
     entry_98.should_receive(:amount_in_pence).and_return("1")
-    entry_98.should_receive(:account_id).and_return("1")
     entry_99 = mock_model(Entry)
-    entry_99.should_receive(:description).and_return("MyString")
+    entry_99.should_receive(:description).and_return("EntryDescription")
     entry_99.should_receive(:paid_on).and_return(Date.today)
     entry_99.should_receive(:amount_in_pence).and_return("1")
-    entry_99.should_receive(:account_id).and_return("1")
 
     assigns[:entries] = [entry_98, entry_99]
 
     @account = mock_model(Account)
+    @account.should_receive(:name).and_return("AccountName")
     assigns[:account] = @account
   end
 
   it "should render list of entries" do
     render "/entries/index.html.erb"
-    response.should have_tag("tr>td", "MyString", 2)
+    response.should have_text(/AccountName/)
+    response.should have_tag("tr>td", "EntryDescription", 2)
     response.should have_tag("tr>td", "1", 2)
   end
 end
--- spec/views/entries/show.html.erb_spec.rb
+++ spec/views/entries/show.html.erb_spec.rb
@@ -5,7 +5,7 @@ describe "/entries/show.html.erb" do
   
   before(:each) do
     @entry = mock_model(Entry)
-    @entry.stub!(:description).and_return("MyString")
+    @entry.stub!(:description).and_return("EntryDescription")
     @entry.stub!(:paid_on).and_return(Date.today)
     @entry.stub!(:amount_in_pence).and_return("1")
     @entry.stub!(:account_id).and_return("1")
@@ -13,13 +13,18 @@ describe "/entries/show.html.erb" do
     assigns[:entry] = @entry
     
     @account = mock_model(Account)
+    @account.stub!(:name).and_return("AccountName")
+
     assigns[:account] = @account
+    
+    @entry.stub!(:account).and_return(@account)
   end
 
   it "should render attributes in <p>" do
     render "/entries/show.html.erb"
-    response.should have_text(/MyString/)
+    response.should have_text(/EntryDescription/)
     response.should have_text(/1/)
+    response.should have_text(/AccountName/)
   end
 end
 
